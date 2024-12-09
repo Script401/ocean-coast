9211de3808e79691e21e1b891d023b29c8a8ea23
JOAO <cg3032159@labA201m06.IFSP>
2024-12-09 15:32:50 -0300

    Responsivo 1.0

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ function linkHover(num) {
     }
     linkCss = document.getElementById(`link-nav-1`).style.cssText
     nums.splice(num-1, 1)
-    document.getElementById(`link-nav-${num}`).style.cssText += 'width: 1000px;height: 350px;transform: rotate(0deg);box-shadow: 10px 10px 10px 10px rgba(1, 34, 31, 0.212);'
+    document.getElementById(`link-nav-${num}`).style.cssText += 'width: '+(window.screen.width > 1000 ? 1000 : 600)+'px;height: 350px;transform: rotate(0deg);box-shadow: 10px 10px 10px 10px rgba(1, 34, 31, 0.212);'
     document.getElementById(`link-nav-titulo-${num}`).style.cssText += 'transform: rotate(0deg);top: 50px;left: 50px;width: 75px;height: 75px;'
     setTimeout(() => {
         document.getElementById(`link-nav-conteudo-${num}`).style.cssText += 'display: block;'
@@ -73,7 +73,7 @@ function linkHover2(num) {
     }
     linkCss = document.getElementById(`link-nav2-1`).style.cssText
     nums.splice(num-1, 1)
-    document.getElementById(`link-nav2-${num}`).style.cssText += 'width: 1000px;height: 450px;transform: rotate(0deg);box-shadow: 10px 10px 10px 10px rgba(1, 34, 31, 0.212);'
+    document.getElementById(`link-nav2-${num}`).style.cssText += 'width: '+(window.screen.width > 1000 ? 1000 : 600)+'px;height: 450px;transform: rotate(0deg);box-shadow: 10px 10px 10px 10px rgba(1, 34, 31, 0.212);'
     document.getElementById(`link-nav2-titulo-${num}`).style.cssText += 'transform: rotate(0deg);top: 50px;left: 50px;width: 75px;height: 75px;'
     setTimeout(() => {
         document.getElementById(`link-nav2-conteudo-${num}`).style.cssText += 'display: block;'
@@ -99,6 +99,7 @@ function linkHover2(num) {
 
 var scroll = window.scrollY
 var scroll2 = (window.scrollY - 100 < 0 ? 0 : window.scrollY - 100)
+var pading = document.getElementById('introducao-div').clientHeight + 700
 var loadInterval = setInterval(() => {
     if (scroll < window.scrollY-20 ) {
         document.getElementById('header').style.top = '-100px'
@@ -108,18 +109,20 @@ var loadInterval = setInterval(() => {
         document.getElementById('header-back').style.top = '-30px'
     }
     scroll = window.scrollY
+    pading = document.getElementById('introducao-div').clientHeight + 700
     scroll2 = ((scroll*2) <= 100 ? 0 : (scroll*2) - 100)
-    1100 - scroll2 < 600 ? document.getElementById('conteiner-preto').style.top = `${600}px` : document.getElementById('conteiner-preto').style.top = `${1100 - scroll2}px`
+    pading - scroll2 < pading - 900 ? document.getElementById('conteiner-preto').style.top = `${pading - 900}px` : document.getElementById('conteiner-preto').style.top = `${pading - scroll2}px`
+    document.getElementById('relevantes-titulo').style.marginTop = (document.getElementById('conteiner-preto').clientHeight-500)+'px'
 }, 100)
 
 var _relevantes = 0
 function relevantesE() {
     _relevantes+1 > 2 ? 0 : _relevantes++
-    _relevantes >= 0 ? document.getElementById('relevantes-e').style.marginLeft = `${1700*_relevantes}px` : 0
-    _relevantes <= 0 ? document.getElementById('relevantes-d').style.marginLeft = `${1700*(Math.abs(_relevantes))}px` : 0
+    _relevantes >= 0 ? document.getElementById('relevantes-e').style.marginLeft = `${2000*_relevantes}px` : 0
+    _relevantes <= 0 ? document.getElementById('relevantes-d').style.marginLeft = `${2000*(Math.abs(_relevantes))}px` : 0
 }
 function relevantesD() {
     _relevantes-1 < -2 ? 0 : _relevantes--
-    _relevantes >= 0 ? document.getElementById('relevantes-e').style.marginLeft = `${1700*_relevantes}px` : 0
-    _relevantes <= 0 ? document.getElementById('relevantes-d').style.marginLeft = `${1700*(Math.abs(_relevantes))}px` : 0
+    _relevantes >= 0 ? document.getElementById('relevantes-e').style.marginLeft = `${2000*_relevantes}px` : 0
+    _relevantes <= 0 ? document.getElementById('relevantes-d').style.marginLeft = `${2000*(Math.abs(_relevantes))}px` : 0
 }
